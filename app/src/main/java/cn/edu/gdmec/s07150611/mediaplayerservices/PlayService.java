634e8bf74d9b9a02a7cc21cc0c9f9d53fa9c80ee
gdmec07150611 <microearth@outlook.com>
2016-11-20 02:49:48 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150611/mediaplayerservices/PlayService.java
@@ -0,0 +1,42 @@
+package cn.edu.gdmec.s07150611.mediaplayerservices;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2016/11/20.
+ */
+public class PlayService extends Service {
+    MediaPlayer mediaPlayer;
+    public PlayService(){
+
+    }
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer=MediaPlayer.create(this,R.raw.exodus);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
+}
